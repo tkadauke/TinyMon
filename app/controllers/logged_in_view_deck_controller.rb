25c9485868dbbf2afd8f4e0756e41a2ec2d66dd7
Thomas Kadauke <tkadauke@imedo.de>
2012-11-21 22:53:20 +0100

    Swipe lefto to right to reveal menu when logged in.
    See https://www.pivotaltracker.com/story/show/39902129

--- app/controllers/logged_in_view_deck_controller.rb
+++ app/controllers/logged_in_view_deck_controller.rb
@@ -1,5 +1,6 @@
 class LoggedInViewDeckController < IIViewDeckController
   def init
+    super
     self.leftController = LoggedInMenuController.alloc.init
     self.centerController = LoggedInNavigationController.alloc.init
     self.leftLedge = 60
