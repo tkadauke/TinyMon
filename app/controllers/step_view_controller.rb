5c9b22aeb8eb3577b29c7ccccaaffb4c58fd3800
Thomas Kadauke <tkadauke@imedo.de>
2012-11-24 19:00:45 +0100

    Delete steps.
    See https://www.pivotaltracker.com/story/show/39898589

--- app/controllers/step_view_controller.rb
+++ app/controllers/step_view_controller.rb
@@ -15,8 +15,19 @@ class StepViewController < Formotion::FormableController
       }]
     )
     
+    self.form.create_section(
+      rows: [{
+        title: "Delete",
+        type: :delete
+      }]
+    )
+    
     self.form.on_submit do
       done_editing
+    end
+    
+    self.form.on_delete do
+      delete
     end
     
     self
@@ -39,6 +50,33 @@ class StepViewController < Formotion::FormableController
         self.step.save do |result|
           SVProgressHUD.dismiss
           if result
+            self.navigationController.popViewControllerAnimated(true)
+          else
+            TinyMon.offline_alert
+          end
+        end
+      end
+    end
+  end
+
+  def delete
+    actionSheet = UIActionSheet.alloc.initWithTitle("Really delete?",
+                                                             delegate:self,
+                                                    cancelButtonTitle:"No",
+                                               destructiveButtonTitle:"Yes, delete",
+                                                    otherButtonTitles:nil)
+  
+    actionSheet.showInView(self.view)
+  end
+  
+  def actionSheet(sender, clickedButtonAtIndex:index)
+    if index == sender.destructiveButtonIndex
+      TinyMon.when_reachable do
+        SVProgressHUD.showWithMaskType(SVProgressHUDMaskTypeClear)
+        self.step.destroy do |result|
+          SVProgressHUD.dismiss
+          if result
+            @parent.steps.delete(self.step)
             self.navigationController.popViewControllerAnimated(true)
           else
             TinyMon.offline_alert
