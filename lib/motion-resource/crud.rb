4556439096eeb906bc17beb149507b279c64f4c6
Thomas Kadauke <tkadauke@imedo.de>
2012-11-25 22:25:36 +0100

    Made block optional in CRUD methods

--- lib/motion-resource/crud.rb
+++ lib/motion-resource/crud.rb
@@ -2,25 +2,25 @@ module RemoteModule
   class RemoteModel
     def save(&block)
       self.class.put(member_url, :payload => { self.class.name.underscore => attributes }) do |response, json|
-        block.call json ? self.class.new(json) : nil
+        block.call json ? self.class.new(json) : nil if block
       end
     end
   
     def create(&block)
       self.class.post(collection_url, :payload => { self.class.name.underscore => attributes }) do |response, json|
-        block.call json ? self.class.new(json) : nil
+        block.call json ? self.class.new(json) : nil if block
       end
     end
   
     def destroy(&block)
       self.class.delete(member_url) do |response, json|
-        block.call json ? self.class.new(json) : nil
+        block.call json ? self.class.new(json) : nil if block
       end
     end
     
     def reload(&block)
       self.class.get(member_url) do |response, json|
-        block.call json ? self.class.new(json) : nil
+        block.call json ? self.class.new(json) : nil if block
       end
     end
   end
