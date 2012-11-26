649deb37c28355afba148432d25ebd4a125d98a4
Thomas Kadauke <tkadauke@imedo.de>
2012-11-26 02:11:01 +0100

    Use motion-support gem

--- spec/motion-support/metaclass_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "metaclass" do
-  it "should return the metaclass for a class" do
-    String.metaclass.is_a?(Class).should == true
-  end
-  
-  it "should return the metaclass for an object" do
-    "string".metaclass.is_a?(Class).should == true
-  end
-end
